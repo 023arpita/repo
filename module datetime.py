79f665970b1c70d901bfa61ba9654055e5f50bbc
023arpita <arpita0267.becse24@chitkara.edu.in>
2025-02-22 19:57:32 +0530

    Update module datetime.py

--- module datetime.py
+++ module datetime.py
@@ -1,4 +1,4 @@
 import datetime
-date_object = datetime.date(2031, 3, 14 )
+date_object = datetime.date(2030, 2, 27 )
 day_name = date_object.strftime("%A")
 print(f"The day name for {date_object} is: {day_name}")
